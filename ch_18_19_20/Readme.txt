d3a94664e316eede8d1a1b67ebcb83f055ee9b4a
Muhammad Yasir <optimisticyasir@outlook.com>
2023-03-14 21:00:09 +0500

    added chapter 18 to 20 assignments

---
+++ ch_18_19_20/Readme.txt
@@ -0,0 +1,4 @@
+Java-script assignments chapter 18 19 20
+
+Make a TODO
+Customize previous assignment "ch_15_16_17" with for loop...
---
+++ ch_18_19_20/index.js
@@ -0,0 +1,18 @@
+// var courses =[];
+// var course1= prompt(course1,"Type your course here");
+// var course2= prompt(course2,"Type your course here");
+// var course3= prompt(course3,"Type your course here");
+// var course4= prompt(course4,"Type your course here");
+// var course5= prompt(course5,"Type your course here");
+
+// courses.push(course1,course2,course3,course4,course5);
+// alert(courses);
+
+// var course1_edit= prompt(course1);
+// var course2_edit= prompt(course2);
+// var course3_edit= prompt(course3);
+// var course4_edit= prompt(course4);
+// var course5_edit= prompt(course5);
+
+// courses.splice(0,5, course1_edit,course2_edit,course3_edit,course4_edit,course5_edit);
+// alert(courses)
